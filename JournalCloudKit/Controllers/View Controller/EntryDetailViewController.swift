120b32da18e41ef82f5abecb3b3eb30daefc8402
Deniz Tutucu <denizsafaktutuncu@gmail.com>
2019-02-25 23:54:05 -0700

    first commit

---
+++ JournalCloudKit/Controllers/View Controller/EntryDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryDetailViewController.swift
+//  JournalCloudKit
+//
+//  Created by Deniz Tutuncu on 2/25/19.
+//  Copyright © 2019 Deniz Tutuncu. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JournalCloudKit/Model/Entry.swift
@@ -0,0 +1,9 @@
+//
+//  Entry.swift
+//  JournalCloudKit
+//
+//  Created by Deniz Tutuncu on 2/25/19.
+//  Copyright © 2019 Deniz Tutuncu. All rights reserved.
+//
+
+import Foundation
